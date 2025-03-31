888562fb3d8dc03ed7ae3c8d1fa5417e66259b6f
Filip Strandberg <filip.strandberg@hyph.com>
2025-03-31 13:43:00 +0200

    create-next-app

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMsHhNCwpmw89jscjIQ9qE1hcFOMY3hEk",
+  authDomain: "umespexarna-tickster.firebaseapp.com",
+  projectId: "umespexarna-tickster",
+  storageBucket: "umespexarna-tickster.firebasestorage.app",
+  messagingSenderId: "260226931316",
+  appId: "1:260226931316:web:3928ba6f79f83ef6250f21"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
