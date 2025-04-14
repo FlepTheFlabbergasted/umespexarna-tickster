09beeff2f74d8d732b8cd41a7149c72ce521bea7
Filip Strandberg <filip.strandberg@hyph.com>
2025-04-14 09:59:34 +0200

    cleanup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,7 +26,7 @@ const SHOW_NAME_PREFIX = 'Alcatraz';
 // GMT+0100 (Central European Time, Standard Time, winter)
 // GMT+0200 (Central European Summer Time)
 const START_DATE = new Date('2025-02-28T00:00:00+01:00'); // Biljetsläpp
-const END_DATE = new Date('2025-04-13T23:59:59+02:00');
+const END_DATE = new Date('2025-04-14T23:59:59+02:00'); // Slutdatum + 1 dag
 
 initializeApp();
 setGlobalOptions({ region: 'europe-west3' });
@@ -99,205 +99,108 @@ exports.getSales = onRequest(async (req, res) => {
 // });
 
 // Manually run the task here https://console.cloud.google.com/cloudscheduler
-exports.addShowAndTicketsSoldRow = onSchedule(
-  { schedule: 'every day 12:00', timeZone: 'Europe/Stockholm' },
-  async () => {
-    const now = new Date();
+exports.addShowAndTicketsSoldRow = exports.addShowAndTicketsSoldRow =
+  onSchedule(
+    {
+      schedule: 'every hour from 08:00 to 00:00',
+      timeZone: 'Europe/Stockholm',
+    },
+    async () => {
+      const now = new Date();
 
-    // Only run if within the date range
-    if (!(now >= START_DATE && now <= END_DATE)) {
-      logger.info(
-        'Skipping function execution. Out of valid date range.',
-        START_DATE,
-        END_DATE
-      );
-      return;
-    }
+      // Only run if within the date range
+      if (!(now >= START_DATE && now <= END_DATE)) {
+        logger.info(
+          'Skipping function execution. Out of valid date range.',
+          START_DATE,
+          END_DATE
+        );
+        return;
+      }
 
-    https
-      .get(TICKSTER_SALES_API_URL, async (incomingHttpMsg) => {
-        const { statusCode } = incomingHttpMsg;
-        const contentType = incomingHttpMsg.headers['content-type'];
+      https
+        .get(TICKSTER_SALES_API_URL, async (incomingHttpMsg) => {
+          const { statusCode } = incomingHttpMsg;
+          const contentType = incomingHttpMsg.headers['content-type'];
 
-        let error;
-        // Any 2xx status code signals a successful response but
-        // here we're only checking for 200.
-        if (statusCode !== 200) {
-          error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType || '')) {
-          error = new Error(
-            'Invalid content-type.\n' +
-              `Expected application/json but received ${contentType}`
-          );
-        }
-
-        if (error) {
-          logger.error(error.message);
-          // Consume response data to free up memory
-          incomingHttpMsg.resume();
-          return;
-        }
-
-        incomingHttpMsg.setEncoding('utf8');
-        let rawData = '';
-        incomingHttpMsg.on('data', (chunk) => {
-          rawData += chunk;
-        });
-
-        incomingHttpMsg.on('end', async () => {
-          try {
-            const parsedData = JSON.parse(rawData);
-            const showsAndTicketsSold = parsedData
-              .filter(
-                (data: any) =>
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Torsdag`) ||
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Fredag`) ||
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Lördag`) ||
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Söndag`)
-              )
-              .reduce(
-                (obj: any, data: any) => ({
-                  ...obj,
-                  [data.name.replace(`${SHOW_NAME_PREFIX} - `, '')]:
-                    data.sales.soldQtyNet,
-                }),
-                {}
-              );
-
-            const newCollection = {
-              date: DateTime.now().toFormat('LLL dd'),
-              millis: DateTime.now().toMillis(),
-              ...showsAndTicketsSold,
-            };
-
-            const collectionRef = await db.collection(COLLECTION_NAME); //.add(newCollection);
-            const snapshot = await collectionRef.get();
-            const previousDoc = snapshot.docs.find(
-              (doc) => doc.data().date === newCollection.date
+          let error;
+          // Any 2xx status code signals a successful response but
+          // here we're only checking for 200.
+          if (statusCode !== 200) {
+            error = new Error(
+              'Request Failed.\n' + `Status Code: ${statusCode}`
             );
-
-            if (previousDoc) {
-              console.log(
-                await collectionRef
-                  .doc(previousDoc.id)
-                  .update({ ...newCollection })
-              );
-            } else {
-              console.log('NEEEEW');
-              await collectionRef.add(newCollection);
-            }
-
-            // If calling manually
-            // res.json(newCollection);
-          } catch (error: any) {
-            logger.error(error.message);
-          }
-        });
-      })
-      .on('error', (error) => {
-        logger.error(error.message);
-      });
-  }
-);
-
-
-// Manually run the task here https://console.cloud.google.com/cloudscheduler
-exports.addShowAndTicketsSoldRowMANUAL = onRequest
-  async (_req,res) => {
-    const now = new Date();
-
-    // Only run if within the date range
-    if (!(now >= START_DATE && now <= END_DATE)) {
-      logger.info(
-        'Skipping function execution. Out of valid date range.',
-        START_DATE,
-        END_DATE
-      );
-      return;
-    }
-
-    https
-      .get(TICKSTER_SALES_API_URL, async (incomingHttpMsg) => {
-        const { statusCode } = incomingHttpMsg;
-        const contentType = incomingHttpMsg.headers['content-type'];
-
-        let error;
-        // Any 2xx status code signals a successful response but
-        // here we're only checking for 200.
-        if (statusCode !== 200) {
-          error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType || '')) {
-          error = new Error(
-            'Invalid content-type.\n' +
-              `Expected application/json but received ${contentType}`
-          );
-        }
-
-        if (error) {
-          logger.error(error.message);
-          // Consume response data to free up memory
-          incomingHttpMsg.resume();
-          return;
-        }
-
-        incomingHttpMsg.setEncoding('utf8');
-        let rawData = '';
-        incomingHttpMsg.on('data', (chunk) => {
-          rawData += chunk;
-        });
-
-        incomingHttpMsg.on('end', async () => {
-          try {
-            const parsedData = JSON.parse(rawData);
-            const showsAndTicketsSold = parsedData
-              .filter(
-                (data: any) =>
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Torsdag`) ||
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Fredag`) ||
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Lördag`) ||
-                  data.name.startsWith(`${SHOW_NAME_PREFIX} - Söndag`)
-              )
-              .reduce(
-                (obj: any, data: any) => ({
-                  ...obj,
-                  [data.name.replace(`${SHOW_NAME_PREFIX} - `, '')]:
-                    data.sales.soldQtyNet,
-                }),
-                {}
-              );
-
-            const newCollection = {
-              date: DateTime.now().toFormat('LLL dd'),
-              millis: DateTime.now().toMillis(),
-              ...showsAndTicketsSold,
-            };
-
-            const collectionRef = await db.collection(COLLECTION_NAME); //.add(newCollection);
-            const snapshot = await collectionRef.get();
-            const previousDoc = snapshot.docs.find(
-              (doc) => doc.data().date === newCollection.date
+          } else if (!/^application\/json/.test(contentType || '')) {
+            error = new Error(
+              'Invalid content-type.\n' +
+                `Expected application/json but received ${contentType}`
             );
-
-            if (previousDoc) {
-              console.log(
-                await collectionRef
-                  .doc(previousDoc.id)
-                  .update({ ...newCollection })
-              );
-            } else {
-              console.log('NEEEEW');
-              await collectionRef.add(newCollection);
-            }
-
-            // If calling manually
-            res.json(newCollection);
-          } catch (error: any) {
-            logger.error(error.message);
           }
+
+          if (error) {
+            logger.error(error.message);
+            // Consume response data to free up memory
+            incomingHttpMsg.resume();
+            return;
+          }
+
+          incomingHttpMsg.setEncoding('utf8');
+          let rawData = '';
+          incomingHttpMsg.on('data', (chunk) => {
+            rawData += chunk;
+          });
+
+          incomingHttpMsg.on('end', async () => {
+            try {
+              const parsedData = JSON.parse(rawData);
+              const showsAndTicketsSold = parsedData
+                .filter(
+                  (data: any) =>
+                    data.name.startsWith(`${SHOW_NAME_PREFIX} - Torsdag`) ||
+                    data.name.startsWith(`${SHOW_NAME_PREFIX} - Fredag`) ||
+                    data.name.startsWith(`${SHOW_NAME_PREFIX} - Lördag`) ||
+                    data.name.startsWith(`${SHOW_NAME_PREFIX} - Söndag`)
+                )
+                .reduce(
+                  (obj: any, data: any) => ({
+                    ...obj,
+                    [data.name.replace(`${SHOW_NAME_PREFIX} - `, '')]:
+                      data.sales.soldQtyNet,
+                  }),
+                  {}
+                );
+
+              const newCollection = {
+                date: DateTime.now().toFormat('LLL dd'),
+                millis: DateTime.now().toMillis(),
+                ...showsAndTicketsSold,
+              };
+
+              const collectionRef = db.collection(COLLECTION_NAME);
+              const snapshot = await collectionRef.get();
+              const previousDoc = snapshot.docs.find(
+                (doc) => doc.data().date === newCollection.date
+              );
+
+              if (previousDoc) {
+                console.log(
+                  await collectionRef
+                    .doc(previousDoc.id)
+                    .update({ ...newCollection })
+                );
+              } else {
+                await collectionRef.add(newCollection);
+              }
+
+              // If calling manually
+              // res.json(newCollection);
+            } catch (error: any) {
+              logger.error(error.message);
+            }
+          });
+        })
+        .on('error', (error) => {
+          logger.error(error.message);
         });
-      })
-      .on('error', (error) => {
-        logger.error(error.message);
-      });
-  }
-);
+    }
+  );
